1f44bf14c57e023c4c77f9c35b96740f2ef7e17c
Gaelen Mcintee <gaelen.mcintee@gfs.com>
2019-03-04 23:25:43 -0500

    start basic server

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
 
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
 // GCP needs this account key filepath added as a system env variable to authenticate.
 // const gcpKeyPath = path.resolve('gcp-service-account-key.json');
 // console.log('Setting environment variable GOOGLE_APPLICATION_CREDENTIALS to ' + gcpKeyPath);
 // process.env.GOOGLE_APPLICATION_CREDENTIALS = path.resolve('gcp-service-account-key.json');
-//
-// const app = express();
+
+const app = express();
 // const transcribeController = require('./controllers/transcribe-controller');
-//
+
 // app.use(express.json({type: 'application/json', limit: "500mb"}));
-//
-// app.route('/transcribe')
-//     .post(transcribeController.transcribe);
-//
-//     app.listen(8080, () => {
-//         console.log('Listening on port 8080...');
-//         });
-//
-//
+
+app.route('/').post((req, res) => {
+    console.log(req);
+});
+
+app.listen(8080, () => {
+    console.log('Listening on port 8080...');
+});
+
